2f94a61ef4fe3eac02059b7659d7d427a5166727
Taska Tamas <tamas.taska@gmx.com>
2023-10-09 17:52:44 +0200

    Added login and token handling

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -1,33 +1,47 @@
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
-import { Ok, Result } from 'src/app/models/utils/result';
+import { Observable, catchError, map, of } from 'rxjs';
+import { Err, Ok, Result, fromJSON } from 'src/app/models/utils/result';
+import { ConfigService } from '../config/config.service';
+import { HttpClient } from '@angular/common/http';
+import { Token } from 'src/app/models/token/token';
+import jwt_decode from 'jwt-decode';
+import { AccessToken } from 'src/app/models/token/accesstoken';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  username: string = "";
-  password: string = "";
-  groups: string[] = [];
-  backendService: any;
-
-  constructor() { }
+  url: string = "";
+  token: Token = new Token();
+  
+  constructor(private httpClient: HttpClient, private config: ConfigService) {
+    this.url = this.config.config.AuthUrl;
+  }
 
   public getUsername(): string {
-    return "";
+    const decoded = jwt_decode<AccessToken>(this.token.access_token);
+    return decoded.preferred_username;
   }
 
   public isLoggedIn(): boolean {
-    return false;
+    return !!this.token.access_token;
   }
 
-  public logout() {}
+  public logout() {
+    this.token = new Token();
+  }
 
 
-  public login(username: string, password: string): boolean {
-    this.username = username;
-    this.password = password;
-    return true;
-        
+  public login(username: string, password: string): Observable<Result<Token>> {
+    const url = this.url + "/login";
+    const body = { Username: username, Password: password };
+    return this.httpClient.post<Token>(url, body).pipe(
+      map(result => {
+        const token = fromJSON<Token>(JSON.stringify(result));
+        this.token = token.unwrap();
+        return new Ok<Token>(this.token);
+      }),
+      catchError(error => of(new Err<Token>(error)))
+    );  
   }
 }
