89320efae1b4afb6781b052b85a565d5d843dccf
Taska Tamas <tamas.taska@gmx.com>
2023-10-16 16:08:40 +0200

    Added token refresh

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -7,6 +7,7 @@ import { Token } from 'src/app/models/token/token';
 import jwt_decode from 'jwt-decode';
 import { AccessToken } from 'src/app/models/token/accesstoken';
 import { Router } from '@angular/router';
+import { Empty } from 'src/app/models/utils/empty';
 
 @Injectable({
   providedIn: 'root'
@@ -45,17 +46,35 @@ export class LoginService {
     this.token = new Token();
   }
 
-
-  public login(username: string, password: string): Observable<Result<Token>> {
+  public login(username: string, password: string): Observable<Result<Empty>> {
     const url = this.url + "/login";
     const body = { Username: username, Password: password };
     return this.httpClient.post<Token>(url, body).pipe(
       map(result => {
         const token = fromJSON<Token>(JSON.stringify(result));
-        this.token = token.unwrap();
-        return new Ok<Token>(this.token);
+        this.saveToken(token.unwrap());
+        return new Ok<Empty>(new Empty());
       }),
-      catchError(error => of(new Err<Token>(error)))
+      catchError(error => of(new Err<Empty>(error)))
     );  
+  }
+
+  public refresh(): Observable<Result<Empty>> {
+    const url = this.url + "/refresh";
+    const body = { RefreshToken: this.token.refresh_token };
+    return this.httpClient.post<Token>(url, body).pipe(
+      map(result => {
+        const token = fromJSON<Token>(JSON.stringify(result));
+        this.saveToken(token.unwrap());
+        return new Ok<Empty>(new Empty());
+      }),
+      catchError(error => of(new Err<Empty>(error)))
+    );  
+  }
+
+  private saveToken(token: Token) {
+    this.token = token;
+    const expire = this.token.expires_in * 1000;
+    setTimeout(() => this.refresh().subscribe(), expire - 10000);
   }
 }
