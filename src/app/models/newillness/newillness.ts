544ce99c9c2d6194bd75a060146019b2a85c9752
saricsacsi <saricsacsi@gmail.com>
2023-10-18 22:15:18 +0200

    csak megcsináltam azt a kurva táblázatot

--- src/app/models/newillness/newillness.ts
+++ src/app/models/newillness/newillness.ts
@@ -7,6 +7,17 @@ export class NewIllness {
     IsMarker: number = 0;
     Defaults: Default[] = [];
 
+    constructor(group: number, groupname: string, alternativenames: string[], ismarker: number, defaults: Default[]) {
+        this.Group = group;
+        this.GroupName = groupname;
+        this.AlternativeNames = alternativenames;
+        this.IsMarker = ismarker;
+        this.Defaults = defaults;
 
-    constructor() { }
+    }
+
+
+
+    
+
 }
