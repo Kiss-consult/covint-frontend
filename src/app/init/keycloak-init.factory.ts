6a719d73df18e8277d4694630d816584093ca7b6
Taska Tamas <tamas.taska@gmx.com>
2023-11-15 11:47:03 +0100

    Added keycloak url configuration

--- src/app/init/keycloak-init.factory.ts
+++ src/app/init/keycloak-init.factory.ts
@@ -1,15 +1,16 @@
 
 import { KeycloakService } from "keycloak-angular";
+import { ConfigService } from "../services/config/config.service";
 
 export function initializeKeycloak(
-  keycloak: KeycloakService
+  keycloak: KeycloakService, configService: ConfigService
   ) {
     return () =>
       keycloak.init({
         config: {
-          url: 'http://localhost:8084',
-          realm: 'Covint',
-          clientId: 'covint-auth'
+          url: configService.config.KeycloakUrl,
+          realm: "Covint",
+          clientId: "covint-auth"
         },
         initOptions: {
           checkLoginIframe: false
--- src/app/models/config/config.ts
+++ src/app/models/config/config.ts
@@ -1,4 +1,5 @@
 export class Config {
     BackendUrl: string = "";
     AuthUrl: string = "";
+    KeycloakUrl: string = "";
 }
